868beea39d990378dcc3a14f82e218627d9cd502
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-28 11:55:09 -0700

    add show trending GIF functionality
    Co-authored-by: Kyle Kay-Perez <Kyle.kayperez@gmail.com>

--- src/main.js
+++ src/main.js
@@ -34,6 +34,39 @@ $('#searchButton').click(function(event) {
   }
 });
 
+$('#trendButton').click(function(event) {
+  event.preventDefault();
+
+  let request = new XMLHttpRequest();
+
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&rating=r`;
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+      console.log(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response) {
+    let responseArray = [0, 1, 2, 3, 4];
+    responseArray.forEach(function(index) {
+      $('#showTrend').append("<img src=" + response.data[index].images.preview_gif.url + " alt='a gif'>");
+    });
+  }
+});
+
+
+
+
+
+
+
+
 
   // try {
   //   if(request.status !== 200) {
