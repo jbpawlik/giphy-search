1c0359a608340df7a7d079e4950141913cb16313
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-28 15:25:50 -0700

    add GIPHY upload functionality
    Co-authored-by: Kyle Kay-Perez <Kyle.kayperez@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 
 import $ from 'jquery';
 import 'bootstrap';
@@ -45,7 +46,6 @@ $('#trendButton').click(function(event) {
     if (this.readyState === 4 && this.status === 200) {
       const response = JSON.parse(this.responseText);
       getElements(response);
-      console.log(response);
     }
   };
 
@@ -60,13 +60,35 @@ $('#trendButton').click(function(event) {
   }
 });
 
+$('#uploadButton').click(function(event){
+  event.preventDefault();
+  let request = new XMLHttpRequest();
+  let sourceURL = $('#fileUpload').val();
+  const url = `https://upload.giphy.com/v1/gifs?api_key=${process.env.API_KEY}&source_image_url=${sourceURL}`;
 
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+      console.log(response);
+    }
+  };
 
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  // request.setRequestHeader('Content-type', 'x-www-form-urlencoded')
+  // request.setRequestHeader(source_image_url, 'https://media1.giphy.com/media/yvXJ5pI97sLg73VQ1h/giphy.gif?cid=ecf05e47lygwx2m4aaozhglmd1y8z2wf4bpm1hqk6xds5n62&rid=giphy.gif&ct=g')
+  // request.setRequestHeader(`https://media1.giphy.com/media/yvXJ5pI97sLg73VQ1h/giphy.gif?cid=ecf05e47lygwx2m4aaozhglmd1y8z2wf4bpm1hqk6xds5n62%26rid=giphy.gif%26ct=g`, key)
+  request.send('');
 
+  function getElements(response) {
+    console.log(response)
 
+    
+  }
 
-
-
+  // https://upload.giphy.com/v1/gifs?api_key=xdohATlHgchpmpttpoNjtdWxKuLWJwUC&source_image_url=https://media1.giphy.com/media/yvXJ5pI97sLg73VQ1h/giphy.gif?cid=ecf05e47lygwx2m4aaozhglmd1y8z2wf4bpm1hqk6xds5n62%26rid=giphy.gif%26ct=g
 
   // try {
   //   if(request.status !== 200) {
@@ -74,6 +96,19 @@ $('#trendButton').click(function(event) {
   //     throw Error;
   //   }
   // } catch(error) {
-  //   console.error('bad input')
+  //   console.error('You are beautiful geniuses and I, the computer, am the one who is wrong!')
   // }
+
+});
+
+
+// https://upload.giphy.com/v1/gifs?api_key=xdohATlHgchpmpttpoNjtdWxKuLWJwUC&source_image_url=https://im6.ezgif.com/tmp/ezgif-6-f4b28f369803.mp4&=&=
+// try {
+//   if(request.status !== 200) {
+//     console.log(request)
+//     throw Error;
+//   }
+// } catch(error) {
+//   console.error('bad input')
+// }
 
