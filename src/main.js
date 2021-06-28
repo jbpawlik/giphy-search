a85638e1fcf21bfdf6db121118b434ebc166dae1
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-28 11:42:36 -0700

    set up GIPHY search application
    Co-authored-by: Kyle Kay-Perez <Kyle.kayperez@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
 
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+// click functionality
+//  let searchObject = input from the search
+$('#searchButton').click(function(event) {
+  event.preventDefault()
+
+  let searchObject = $('#searchInput').val();
+
+  let request = new XMLHttpRequest();
+
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchObject}&limit=5&offset=0&rating=g&lang=en`;
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+      console.log(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response) {
+    let responseArray = [0, 1, 2, 3, 4]
+    responseArray.forEach(function(index) {
+      $('#showGif').append("<img src=" + response.data[index].images.preview_gif.url + " alt='a gif'>");
+    });
+  }
+});
+
+
+  // try {
+  //   if(request.status !== 200) {
+  //     console.log(request)
+  //     throw Error;
+  //   }
+  // } catch(error) {
+  //   console.error('bad input')
+  // }
+
